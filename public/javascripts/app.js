3e812d0a2a429288a62b2220f09ce394a4ee64ec
Mounika <212687639@logon.ds.ge.com>
2018-02-24 17:29:24 +0530

    turning angular

---
+++ public/javascripts/app.js
@@ -0,0 +1,12 @@
+var app = angular.module('enotes',['ngRoute']);
+app.component('homepage',{
+  templateUrl : './html/homepage.html'
+});
+
+app.component('signup',{
+  templateUrl : './html/signup.html'
+});
+
+app.componnet('login',{
+  templateUrl : './html/login.html'
+});

34eb41ebb948df3fe578e8ad6ca9be8a79b00e02
Mounika <212687639@logon.ds.ge.com>
2018-02-24 16:08:47 +0530

    added  public directory

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 const PORT = process.env.PORT || 5000
+app.use(express.static(path.join(__dirname,'public')));
  app.get('/',function(req,res){
-   res.send("Hello World");
+   res.sendFile(path.join(__dirname,'public/html/index.html'));
  });
 
  app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
